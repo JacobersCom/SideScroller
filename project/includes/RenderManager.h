4eba22ac2835a1e5af4786ae9ec4f632fdc9e34b
JacobersCom <jacobymartin1@icloud.com>
2025-11-30 22:53:27 -0500

    Setting up rendermanager with static function, that will create an instance of the manager once before main is called, than all other manager that this manager depends on will be called

--- project/includes/RenderManager.h
+++ project/includes/RenderManager.h
@@ -3,9 +3,16 @@
 #include "Defines.h"
 #include "WindowManager.h"
 
-class RendererManager 
+class RenderManager 
 {
+public:
+
 	bool Initialize(SDL_Window* window);
+	RenderManager();
+	~RenderManager();
+
+	//Get the one and only instance
+	static RenderManager& get();
 
 private:
 
