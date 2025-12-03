a56120058002837bac02725c284be0b42f387047
JacoberCom <jacobymartin1@icloud.com>
2025-12-02 21:59:32 -0500

    Actor class created.

--- Actor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Actor
-{
-	//called every frame to update the actor
-	virtual void Update(float deltaTime);
-	//called every frame to draw the actor
-	virtual void Draw();
-};
-
--- project/includes/Defines.h
+++ project/includes/Defines.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "SDL3/SDL.h"
+#include <iostream>
+#include <vector>
 
 constexpr int screen_W = 800;
 constexpr int screen_H = 400;
 
-
+struct vec2
+{
+	float x, y;
+};
 
 
