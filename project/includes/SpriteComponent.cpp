d7b70c23dd012c6dbdbf77dc18d9935a3f8023a2
JacobersCom <jacobymartin1@icloud.com>
2025-12-11 00:45:35 -0500

    Beginning to get into sprits!!!!

---
+++ project/includes/SpriteComponent.cpp
@@ -0,0 +1,21 @@
+#include "SpriteComponent.h"
+
+SDL_Texture* SpriteComponent::LoadTexture(const char* fileName)
+{
+	SDL_Surface* surf = IMG_Load(fileName);
+
+	if (!surf)
+	{
+		SDL_Log("ERROR Failed to load surface %s", fileName);
+		return nullptr;
+	}
+
+	SDL_Texture* text = SDL_CreateTextureFromSurface(mRenderManager->renderer, surf);
+	SDL_DestroySurface(surf);
+	if (!text)
+	{
+		SDL_Log("ERROR: Failed to convert surface to texture for %s", fileName);
+		return nullptr
+	}
+	return text;
+}
