58f2a56bb9bda58927d248f322756327333d011d
JacobersCom <jacobymartin1@icloud.com>
2025-12-21 16:02:21 -0500

    Updated actor class

--- project/includes/Actor.cpp
+++ project/includes/Actor.cpp
@@ -2,31 +2,62 @@
 
 Actor::Actor(Root* game)
 {
+	this->mRoot = game;
+	this->mState = State::EActive; // The actor is active
+	this->mPosition = { 0.0f, 0.0f };
+	this->mRotation = 0.0f;
+	this->mScale = 1.0f;
 
+	mRoot->AddActor(this); // add this actor to the game
 }
 
 Actor::~Actor()
 {
-
+	//Remove this actor and while the component list is not empty delete any components in the list
+	mRoot->RemoveActor(this);
+	while (!this->mComponents.empty())
+	{
+		delete this->mComponents.back();
+	}
 }
 
 void Actor::Update(float deltaTime)
 {
-
+	while (State::EActive)
+	{
+		this->UpdateComponents(deltaTime);
+		this->UpdateActor(deltaTime);
+	}
 }
 
 void Actor::UpdateComponents(float deltaTime)
 {
+	for (auto c : mComponents)
+	{
+		c->Update(deltaTime); // Update the componets
+	}
 }
 
 void Actor::UpdateActor(float deltaTime)
 {
+
 }
 
 void Actor::AddComponent(Component* comp)
 {
+	auto iter = this->mComponents.begin();
+	for (; iter != mComponents.end(); ++iter)
+	{
+		if (comp->GetUpdateOrder() < (*iter)->GetUpdateOrder());
+		break;
+	}
+	
 }
 
 void Actor::RemoveComponent(Component* comp)
 {
+	//Find this comp in the list of comps
+	auto iter = std::remove(this->mComponents.begin(), this->mComponents.end(), comp);
+	//Remove this comp and make it the new end of the list
+	this->mComponents.erase(iter, mComponents.end());
 }
